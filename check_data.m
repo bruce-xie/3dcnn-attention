a6d2fa56fcb99290ab886f6d1dfedffa6d0bed7d
hope <hope-yao@asu.edu>
2016-10-03 21:55:04 -0700

    check training data

---
+++ check_data.m
@@ -0,0 +1,37 @@
+clear; close all;
+threshold = 0.5;
+ddir = '/home/p2admin/Documents/Hope/voxnet/more_data_real_10_10_1_0/volumetric_data';
+
+dd1 = [ddir,'/more_pot/30/train'];
+ff1 = dir(dd1);
+for i=3:size(ff1,1)
+    fff=[dd1,'/',ff1(i).name];
+    load(fff);
+    cnt = i-2;
+    subplot(4,5,cnt);show_sample(instance ,threshold);view(2)
+end
+
+dd2 = [ddir,'/more_pot/30/test'];
+ff2 = dir(dd2);
+for i=3:size(ff2,1)
+    fff=[dd2,'/',ff2(i).name];
+    load(fff);
+    subplot(4,5,10);show_sample(instance ,threshold);view(2)
+end
+
+dd3 = [ddir,'/more_cup/30/train'];
+ff3 = dir(dd3);
+for i=3:size(ff3,1)
+    fff=[dd3,'/',ff3(i).name];
+    load(fff);
+    cnt = 10+i-2;
+    subplot(4,5,cnt);show_sample(instance ,threshold);view(2)
+end
+
+dd4 = [ddir,'/more_cup/30/test'];
+ff4 = dir(dd4);
+for i=3:size(ff4,1)
+    fff=[dd4,'/',ff4(i).name];
+    load(fff);
+    subplot(4,5,20);show_sample(instance ,threshold);view(2)
+end
